1a2ea3990c078cdb81c1f72937541aacb9e0ff7c
Cho Yoongeon <tesung5470@naver.com>
2023-06-16 19:10:16 +0900

    피드백 반영중

--- movielist.js
+++ movielist.js
@@ -1,27 +1,32 @@
+import apikey from "./apikey.js";
+
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZmU0ZmQ2OTExMjk4YTk1MGI3ODhlNjk0ODVkZjZmZiIsInN1YiI6IjY0NzU4M2Y1Njc0M2ZhMDExOTdhNzA1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.exgR1edP5kPFmM8OjkVJfy8DoRlmQMJV4bQ_Msz6pBc",
+    Authorization: apikey.aut,
   },
 };
 
-function Moviefiltering() {
-  fetch(
-    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-    options
-  )
+function MovieListing() {
+  fetch(apikey.apiUrl, options)
     .then((response) => response.json())
-    // .then((response) => console.log(response))
     .then((data) => {
-      const movies = data.results; // 영화 목록 추출
+      const movies = data.results;
+      const movieArea = document.getElementById("cards-box");
 
-      const movieArea = document.getElementById("cards-box"); // 영화를 추가할 HTML 영역 선택
-
-      const movInput = document.getElementById("movInput").value; // input 속성 받아오기
+      // 카드 클릭 이벤트
+      movieArea.addEventListener("click", (event) => {
+        const card = event.target.closest(".card");
+        if (card) {
+          const id = card.id;
+          showId(id);
+        }
+      });
 
       let html = ""; // HTML 템플릿 문자열 초기화
+
+      const movInput = document.getElementById("movInput").value; // input 속성 받아오기
 
       const filteredMovies = movies.filter((movie) => {
         return movie.title.toLowerCase().includes(movInput.toLowerCase());
@@ -29,16 +34,16 @@ function Moviefiltering() {
 
       filteredMovies.forEach((movie) => {
         html += `
-                                  <div class="card" onclick="showId(${movie.id})">
-                                  <img
-                                  src= "https://image.tmdb.org/t/p/w500/${movie.poster_path}"
-                                      class="card-img-top"
-                                  />
-                                  <div class="card-body">
-                                  <h5 class="card-title">${movie.title}</h5>
+                                  <div class="card" id="${movie.id}">  
+                                    <img
+                                    src= "https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+                                        class="card-img-top"
+                                    />
+                                    <div class="card-body">
+                                      <h5 class="card-title">${movie.title}</h5>
                                       <p class="card-text">${movie.overview}</p>
                                       <p>rate : ${movie.vote_average}</p>
-                                  </div>
+                                    </div>
                                   </div>
                               `; // HTML 템플릿에 카드 추가
       });
@@ -48,21 +53,29 @@ function Moviefiltering() {
     .catch((err) => console.error(err));
 }
 
-Moviefiltering();
+MovieListing();
 
-function showId(id) {
-  alert("영화 id: " + id);
-} // 카드 선택 시 di 보여주기
-
+// window load 시 검색창 고정
 window.addEventListener("load", function () {
   const searchInput = document.getElementById("movInput");
-  searchInput.focus(); // window load 시 검색창 고정
+  searchInput.focus();
 });
 
+// Enter key 기능
 document.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-    // 실행할 기능 작성
-    Moviefiltering();
-    // Enter key 기능
+    MovieListing();
   }
 });
+
+// 검색 버튼 이벤트
+searchButton.addEventListener("click", () => {
+  MovieListing();
+});
+
+// id 보여주는 이벤트
+function showId(id) {
+  alert("영화 id: " + id);
+}
+
+const search = () => {};
